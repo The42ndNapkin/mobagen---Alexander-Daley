5aa6f217d61347e0e694a02778ac7da9bf31d7d6
AlexanderDaley <alexander.daley@mymail.champlain.edu>
2024-09-02 11:36:00 -0400

    Started work on separation and cohesion rules

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -6,8 +6,26 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
+  Vector2f position = boid->getPosition();
+  int neighbors = 0;
+  Vector2f CoM = boid->getPosition();
+
+  for(Boid* b : neighborhood) {
+    if(b->getPosition() == position) {
+      continue;
+    }
+    CoM += b->getPosition();
+    neighbors++;
+  }
 
   // find center of mass
+  CoM /= neighbors;
+  Vector2f cohesionPos = Vector2f(CoM.x * position.x, CoM.y * position.y);
+  cohesionForce = cohesionPos/boid->getDetectionRadius();
 
+  cohesionForce = cohesionForce.normalized();
+  if(cohesionForce.getMagnitude() > 0.2f) {
+    cohesionForce*=0.2f;
+  }
   return cohesionForce;
 }
--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -10,14 +10,27 @@ Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Bo
   //    float desiredDistance = desiredMinimalDistance;
   //
   //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-  //    if (!neighborhood.empty()) {
-  //        Vector2f position = boid->transform.position;
-  //        int countCloseFlockmates = 0;
+      if (!neighborhood.empty()) {
+          Vector2f position = boid->transform.position;
+        //Get info from all neighbors
+        for(const Boid* b : neighborhood) {
+          if(b->getPosition() == position) {
+            continue;
+          }
+          Vector2f separationVector = boid->getPosition() - b->getPosition();
+          float distance = sqrt((separationVector.x*separationVector.x) + (separationVector.y*separationVector.y));
+          if(distance < desiredMinimalDistance) {
+            //Add force inverse to distance to separation Force
+            separatingForce = separatingForce + separationVector;
+          }
+        }
   //        // todo: find and apply force only on the closest mates
-  //    }
+      }
 
   separatingForce = Vector2f::normalized(separatingForce);
-
+  if(separatingForce.getMagnitude() > 0.2f) {
+    separatingForce = separatingForce*0.2f;
+  }
   return separatingForce;
 }
 
