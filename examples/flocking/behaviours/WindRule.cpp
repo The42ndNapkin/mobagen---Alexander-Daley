9e37ee616f55ad363d20e6c8194db1cb5ba13cf7
AlexanderDaley <alexander.daley@mymail.champlain.edu>
2024-09-09 10:03:30 -0400

    Late fix apparently for windrule apparently windDirection was already in radians

--- examples/flocking/behaviours/WindRule.cpp
+++ examples/flocking/behaviours/WindRule.cpp
@@ -7,7 +7,7 @@
 
 Vector2f WindRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
     //Converts windAngle to radians for calculations
-  Vector2f windForce = Vector2f(sin((windAngle * (M_PI / 180))), cos((windAngle * (M_PI / 180))));
+  Vector2f windForce = Vector2f(sin((windAngle)), cos((windAngle)));
   //returns force multiplied by the weight of the wind
   windForce = windForce.normalized();
   //Not sure why but the force isn't working properly.  For some reason the updated wind angle doesn't update force direction and the calcs aren't proper?
