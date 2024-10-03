643fd47703c7063c38bd8af455a0298eb13de87e
AlexanderDaley <alexander.daley@mymail.champlain.edu>
2024-10-03 11:21:24 -0400

    Initial commit

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -18,10 +18,22 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 
   while (!frontier.empty()) {
     // get the current from frontier
-    // remove the current from frontierset
+    Point2D current = frontier.front();
+    if (frontierSet.contains(current))
+    {
+        // remove the current from frontierset
+        frontierSet.erase(current);
+    }
     // mark current as visited
+    visited[current] = true;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    std::vector<Point2D> neighbors = getVisitableNeighbors(w, current);
     // iterate over the neighs:
+    for (Point2D neighbor: neighbors) {
+      if (visited[neighbor] == false) {
+      
+      }
+    }
     // for every neighbor set the cameFrom
     // enqueue the neighbors to frontier and frontierset
     // do this up to find a visitable border and break the loop
@@ -31,4 +43,25 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
   return vector<Point2D>();
+}
+
+std::vector<Point2D> Agent::getVisitableNeighbors(World* w, Point2D p) { 
+    auto sideOver2 = w->getWorldSideSize() / 2;
+    std::vector<Point2D> neighbors;
+
+    // Check all sides
+    if (p.Up().y >= sideOver2 && w->getContent(p.Up()) == false) {
+      neighbors.push_back(p.Up());
+    }
+    if (p.Right().x <= sideOver2 && w->getContent(p.Right()) == false) {
+      neighbors.push_back(p.Right());
+    }
+    if (p.Down().y <= sideOver2 && w->getContent(p.Down()) == false) {
+      neighbors.push_back(p.Down());
+    }
+    if (p.Left().x >= sideOver2 && w->getContent(p.Left()) == false) {
+      neighbors.push_back(p.Left());
+    }
+
+    return neighbors;
 }
